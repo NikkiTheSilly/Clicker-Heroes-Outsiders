4431674513d28a7e7ca527441e0a5ab1766025af
Nikki <funkytrickster1@gmail.com>
2025-10-18 16:27:47 +1100

    borb disabled maybe
    Set borbTarget to 0 instead of calculating it based on b.

--- js/main.js
+++ js/main.js
@@ -199,7 +199,7 @@ function refresh(test, ancientSouls, simulating) {
     } else {
         let nonBorb = ancientSouls > 433000 ? 500 : 1000;
         let b = this.spendAS(1, ancientSouls - nonBorb);
-        borbTarget = b * 5000;
+        borbTarget = 0;
         if (b > 1026) {
             this.newHze = 5.46e6;
         } else if (borbToZone[b]) {
